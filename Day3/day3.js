1793eac9d491d441d56507fed49af22bdca5b833
Lucie Cicvarkova <46781332+LucieCicvarkova@users.noreply.github.com>
2022-12-03 22:37:41 +0100

    Add files via upload

--- Day3/day3.js
+++ Day3/day3.js
@@ -2,12 +2,11 @@ const fs = require("fs");
 const input = fs.readFileSync("./day3-input.txt", "utf8");
 const inputArray = input.split("\r\n"); 
 
-let sumOfSharedItems = 0;
-
+/* ---part one--- */
 const findSharedItem = (row) => {
     let sharedItem = "";
-    let firstCompartment = row.slice(0,row.length/2+1);
-    let secondCompartment = row.slice(row.length/2+1);    
+    let firstCompartment = row.slice(0,row.length/2);
+    let secondCompartment = row.slice(row.length/2);    
     for(let i = 0; i < firstCompartment.length; i++ ) {
             for(let j = 0; j < secondCompartment.length; j++) {
                 if(firstCompartment[i] === secondCompartment[j]) {
@@ -23,13 +22,48 @@ const transformIntoNumber = (letter, alphabet) => {
     return alphabet.indexOf(letter);
     }
 
+let sumOfSharedItems = 0;
+
 inputArray.forEach(function(row) {
-    let sharedItemAsLetter = findSharedItem(row);
-    console.log("Shared letter is: " + sharedItemAsLetter);
-    let sharedItemAsNumber = transformIntoNumber(sharedItemAsLetter, alphabet);
-    console.log("Shared number is: " + sharedItemAsNumber)
-    sumOfSharedItems = sumOfSharedItems + sharedItemAsNumber; 
-    console.log("Sum is: " + sumOfSharedItems);
+    let sharedItemAsLetter = findSharedItem(row);    
+    let sharedItemAsNumber = transformIntoNumber(sharedItemAsLetter, alphabet);    
+    sumOfSharedItems = sumOfSharedItems + sharedItemAsNumber;     
 });    
 
-console.log(sumOfSharedItems);
+console.log("Sum of shared items: " +sumOfSharedItems);
+
+/*---part two---*/
+
+let groupsArray = [];
+const getGroups = (array) => {
+    for(let r = 0; r < array.length; r += 3) {
+        const group = array.slice(r, r + 3);
+        groupsArray.push(group);
+    }
+    return groupsArray;
+}
+getGroups(inputArray);
+console.log(groupsArray);
+
+let arrayOfBadges = [];
+const getArrayOfBadges = (group) => {
+    for(let p = 0; p < group[0].length; p++) {
+        let possibleBadge = group[0][p];        
+        (group[1].includes(possibleBadge) && group[2].includes(possibleBadge)) ? arrayOfBadges.push(possibleBadge) : false;
+    }
+    return arrayOfBadges;
+}
+
+groupsArray.forEach(function(group) {
+    getArrayOfBadges(group);
+});
+console.log(arrayOfBadges);
+
+const arrayOfNumbers = arrayOfBadges.map(badge => {
+    return transformIntoNumber(badge, alphabet);
+})
+
+console.log(arrayOfNumbers);
+
+const badgesSum =  arrayOfNumbers.reduce((acc, val) => acc + val);
+console.log(badgesSum);
