0ac7b5dc5094d186aa5dfa63795d6b93cf5f0a20
Lucie Cicvarkova <46781332+LucieCicvarkova@users.noreply.github.com>
2022-12-05 16:12:50 +0100

    Add files via upload

--- Day4/day4.js
+++ Day4/day4.js
@@ -1,38 +1,79 @@
 const fs = require("fs");
 const input = fs.readFileSync("./day4-input.txt", "utf8");
-const inputArray = input.split("\r\n"); 
+const inputArray = input.split("\r\n");
 
 const nonDigits = new RegExp(/\D/);
 const arrayOfNumbers = inputArray.map((item) => {
-    return item.split(nonDigits);
-})
+  return item.split(nonDigits);
+});
 
-/*console.log(arrayOfNumbers);*/
+let countOfCompleteOverlaps = 0;
+const countCompleteOverlaps = (item) => {
+  if (
+    (parseInt(item[0]) > parseInt(item[2]) &&
+      parseInt(item[1]) > parseInt(item[3])) ||
+    (parseInt(item[0]) < parseInt(item[2]) &&
+      parseInt(item[1]) < parseInt(item[3]))
+  ) {
+    /* do nothing */
+  } else {
+    countOfCompleteOverlaps++;
+  }
+};
 
-const testingArray = [
-[ '5', '5', '5', '5' ],
-[ '5', '10', '5', '10' ],
-[ '5', '10', '5', '5' ],
-[ '5', '5', '5', '10' ],
-[ '1', '10', '2', '9' ],
-[ '2', '9', '1', '10' ],
-[ '4', '9', '5', '10' ],
-[ '5', '10', '4', '9' ],
-]
+let countOfPartialOverlaps = 0;
+const countPartialOverlaps = (item) => {
+  if (
+    parseInt(item[1]) >= parseInt(item[2]) &&
+    parseInt(item[0]) <= parseInt(item[3])
+  ) {
+    countOfPartialOverlaps++;    
+  } 
+};
 
-let countOfOverlaps = 0;
-const countOverlaps = (item) => {    
-if (parseInt(item[0]) > parseInt(item[2]) && parseInt(item[1]) > parseInt(item[3]) || parseInt(item[0]) < parseInt(item[2]) && parseInt(item[1]) < parseInt(item[3])  ) {
-    console.log("No overlap");
-}
-else {
-    countOfOverlaps++;
-    console.log("Count of overlaps: " + countOfOverlaps);
-}
-}
 
-arrayOfNumbers.map((item) => {
-        return countOverlaps(item);  
-    } )     
+arrayOfNumbers.forEach((item) => {
+    countCompleteOverlaps(item);   
+});
+console.log(countOfCompleteOverlaps);
 
-console.log(countOfOverlaps);
+/* --- part two --- */
+
+arrayOfNumbers.forEach((item) => {
+    countPartialOverlaps(item);   
+});
+console.log(countOfPartialOverlaps);
+
+/* ---Test--- 
+
+const testCompleteOverlap = [
+  ["5", "5", "5", "5"],
+  ["5", "10", "5", "10"],
+  ["5", "10", "5", "5"],
+  ["5", "5", "5", "10"],
+  ["1", "10", "2", "9"],
+  ["2", "9", "1", "10"],
+  ["4", "9", "5", "10"],
+  ["5", "10", "4", "9"],
+];
+
+const testPartialOverlap = [
+  ["1", "5", "5", "10"],
+  ["5", "10", "1", "5"],
+  ["1", "5", "4", "6"],
+  ["4", "6", "1", "5"],
+  ["1", "5", "1", "6"],
+  ["1", "6", "1", "5"],
+  ["1", "5", "6", "10"],
+  ["6", "10", "1", "5"],
+];
+
+testCompleteOverlap.forEach((item) => {
+    countCompleteOverlaps(item);   
+});
+console.log(countOfCompleteOverlaps);
+
+testPartialOverlap.forEach((item) => {
+    countPartialOverlaps(item);   
+});
+console.log(countOfPartialOverlaps); */
