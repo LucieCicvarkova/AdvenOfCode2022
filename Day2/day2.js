b8485437b4252686423f2b09f5e37d86674ed609
Lucie Cicvarkova <46781332+LucieCicvarkova@users.noreply.github.com>
2022-12-06 21:09:45 +0100

    Update day2.js

--- Day2/day2.js
+++ Day2/day2.js
@@ -2,58 +2,58 @@ const fs = require("fs");
 const input = fs.readFileSync("./day2-input.txt", "utf8");
 const inputArray = input.split("\r\n"); 
 
+/* Rock-scissors-paper
 
-// for(let i = 0; i < inputArray.length; i++) {
-//     switch (inputArray[i]) {
-//         case 'A X': inputArray[i] = 4;
-//         break;
-//         case 'A Y': inputArray[i] = 8;
-//         break;
-//         case 'A Z': inputArray[i] = 3;
-//         break;
-//         case 'B X': inputArray[i] = 1;
-//         break;
-//         case 'B Y': inputArray[i] = 5;
-//         break;
-//         case 'B Z': inputArray[i] = 9;
-//         break;
-//         case 'C X': inputArray[i] = 7;
-//         break;
-//         case 'C Y': inputArray[i] = 2;
-//         break;
-//         case 'C Z': inputArray[i] = 6;
-//         break;
-//         default: console.log("Not expected input");
-//         break;
-//     }
-// }
+A,B,C - oponnent (A-rock, B-paper, C-scissors)
+X,Y,Z - me (X - rock, Y - paper, Z - scissors)
+scoring system:
+ win +6
+ draw + 3
+ loose 0
+ me choosing Rock +1
+ me chosing Paper +2
+ me choosing Scissors +3
+*/
 
-//part two
+const getResultOfGame1 = (round) => {
+     switch (round) {
+         case 'A X': return 4;        
+         case 'A Y': return 8;         
+         case 'A Z': return 3;        
+         case 'B X': return 1;         
+         case 'B Y': return 5;         
+         case 'B Z': return 9;         
+         case 'C X': return 7;         
+         case 'C Y': return 2;        
+         case 'C Z': return 6;         
+     }
+ }
 
-for(let i = 0; i < inputArray.length; i++) {
-    switch (inputArray[i]) {
-        case 'A X': inputArray[i] = 3;
-        break;
-        case 'A Y': inputArray[i] = 4;
-        break;
-        case 'A Z': inputArray[i] = 8;
-        break;
-        case 'B X': inputArray[i] = 1;
-        break;
-        case 'B Y': inputArray[i] = 5;
-        break;
-        case 'B Z': inputArray[i] = 9;
-        break;
-        case 'C X': inputArray[i] = 2;
-        break;
-        case 'C Y': inputArray[i] = 6;
-        break;
-        case 'C Z': inputArray[i] = 7;
-        break;
-        default: console.log("Not expected input");
-        break;
-    }
-}
+const arrayAfterGame1 = inputArray.map((item) => getResultOfGame1(item));
+const summedArray1 =  arrayAfterGame1.reduce((acc, val) => acc + val);
+console.log(summedArray1);
 
-const summedArray =  inputArray.reduce((acc, val) => acc + val, 0);
-console.log(summedArray);
+/*  ---part two---
+New scoring system:
+X = me choosing shape to loose
+Y = me choosing shape to draw
+Z = me choosing shape to win
+*/
+
+    const getResultOfGame2 = (round) => {        
+            switch (round) {
+                case 'A X': return 3;               
+                case 'A Y': return 4;               
+                case 'A Z': return 8;                
+                case 'B X': return 1;                
+                case 'B Y': return 5;               
+                case 'B Z': return 9;                
+                case 'C X': return 2;                
+                case 'C Y': return 6;                
+                case 'C Z': return 7;              
+            }  
+        }
+
+const arrayAfterGame2 = inputArray.map((round) => getResultOfGame2(round)); 
+const summedArray2 =  arrayAfterGame2.reduce((acc, val) => acc + val);
+console.log(summedArray2);
